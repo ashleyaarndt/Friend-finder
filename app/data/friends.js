f5a69b8b65418505e006d5b1d3962e4f27deae4e
ashley <ashleyaarndt@outlook.com>
2019-04-16 20:31:46 -0400

    Fix bugs

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,40 +6,39 @@ var friends = [
   },
   {
     name: "Scooby",
-    photo:
-      "scoob.png"
+    photo: "scoob.png",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-  }
-  name: "Shaggy",
-    photo:
-      "shaggy.png",
+  },
+  {
+    name: "Shaggy",
+    photo: "shaggy.png",
     scores: [5, 1, 3, 4, 2, 1, 2, 3, 3, 1]
+  },
+  {
+    name: "Velma",
+    photo: "velma.png",
+    scores: [5, 5, 5, 5, 3, 3, 3, 3, 3, 1]
+  },
+  {
+    name: "Freddy",
+    photo: "freddy.png",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Daphne",
+    photo: "daphne.png",
+    scores: [1, 2, 3, 4, 5, 1, 1, 5, 3, 4]
+  },
+  {
+    name: "Black Knight",
+    photo: "knight.png",
+    scores: [5, 2, 4, 4, 5, 1, 2, 5, 2, 2]
+  },
+  {
+    name: "Captain Cutler",
+    photo: "cap.png",
+    scores: [5, 2, 4, 4, 3, 3, 3, 2, 4, 1]
   }
-  name: "Velma",
-  photo:
-    "velma.png",
-  scores: [5, 5, 5, 5, 3, 3, 3, 3, 3, 1]
-}
-name: "Freddy",
-photo:
-  "freddy.png",
-scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-}
-name: "Daphne",
-photo:
-  "daphne.png",
-scores: [1, 2, 3, 4, 5, 1, 1, 5, 3, 4]
-}
-name: "Black Knight",
-photo:
-  "knight.png",
-scores: [5, 2, 4, 4, 5, 1, 2, 5, 2, 2]
-}
-name: "Captain Cutler",
-photo:
-  "cap.png",
-scores: [5, 2, 4, 4, 3, 3, 3, 2, 4, 1]
-}
 ];
 
 module.exports = friends;
