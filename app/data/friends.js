5c44b7029ae48ec7d5a6d6d2386a2f074ebe5c4d
ashley <ashleyaarndt@outlook.com>
2019-04-16 20:10:55 -0400

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,45 @@
 var friends = [
   {
-    name: "Ash",
-    photo: "pic.html",
+    name: "Miner Forty Niner",
+    photo: "miner.png",
     scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
   },
   {
-    name: "Ahmed",
+    name: "Scooby",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "scoob.png"
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   }
+  name: "Shaggy",
+    photo:
+      "shaggy.png",
+    scores: [5, 1, 3, 4, 2, 1, 2, 3, 3, 1]
+  }
+  name: "Velma",
+  photo:
+    "velma.png",
+  scores: [5, 5, 5, 5, 3, 3, 3, 3, 3, 1]
+}
+name: "Freddy",
+photo:
+  "freddy.png",
+scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}
+name: "Daphne",
+photo:
+  "daphne.png",
+scores: [1, 2, 3, 4, 5, 1, 1, 5, 3, 4]
+}
+name: "Black Knight",
+photo:
+  "knight.png",
+scores: [5, 2, 4, 4, 5, 1, 2, 5, 2, 2]
+}
+name: "Captain Cutler",
+photo:
+  "cap.png",
+scores: [5, 2, 4, 4, 3, 3, 3, 2, 4, 1]
+}
 ];
 
 module.exports = friends;
